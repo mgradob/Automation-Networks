04133bc21b5ac6795126342c84ea3444f5921d43
Miguel Grado <mgradob@gmail.com>
2016-02-13 09:53:50 -0700

    Fixed dragomans, added run task helloworld.

--- Automation Networks.X/config.h
+++ Automation Networks.X/config.h
@@ -12,17 +12,20 @@
 #include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // CONFIG
-#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT/T1OSO and RA7/OSC1/CLKIN/T1OSI)
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT/T1OSO pin, I/O function on RA7/OSC1/CLKIN/T1OSI)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
 #pragma config PWRTE = OFF      // Power Up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config MCLRE = OFF       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
 #pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
 #pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
 #pragma config BOREN = OFF      // Brown-out Reset Selection bits (BOR disabled)
 #pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+
+#define _XTAL_FREQ 8000000
 
 /**
  * General MCU configuration.
--- Automation Networks.X/main.c
+++ Automation Networks.X/main.c
@@ -5,8 +5,12 @@
  * Created on January 27, 2016, 11:24 PM
  */
 
+#include "config.h"
+
 #include "timer0.h"
 #include "uart.h"
+
+#include "task_helloworld.h"
 
 unsigned long msCount = 0; 
 
@@ -14,10 +18,14 @@ unsigned long msCount = 0;
  * ISR for the project.
  */
 void interrupt mIsr(void){
-    if(INTCONbits.T0IE && INTCONbits.T0IF){
-        INTCONbits.T0IF = 0;
+    if(T0IF){
+        T0IF = 0;
         
         msCount++;
+    }
+    
+    if(RCIF){
+        saveByte(RCREG);
     }
 }
 
@@ -28,18 +36,21 @@ void main(void){
     configMcu();
     
     while(1){
-        if(msCount == 1000){
-            msCount = 0;
-            
-            sendstr("HOLA MUNDO\n\r");
-        }
+        run_helloworld(msCount);
     }
 }
 
 void configMcu(void){
-    // Configure Timer0
+    // Configure MCU
+    OSCCON = 0b01110001;
+    ANSEL = 0x00;
+    TRISB = 0x00;
+    
+    // Configure modules
     timer0_config();
     uart_config();
     
-    INTCONbits.GIE = 1;
+    // Global & peripherial interrupts
+    PEIE = 1;
+    GIE = 1;
 }
